6f73fab20ad30cd961dc07f8d1a64e976d13febe
GSperandio <gsperandio@pr.sebrae.com.br>
2023-09-15 16:27:24 -0300

    First Commit

---
+++ app/src/main/java/com/example/highbee/model/User.java
@@ -0,0 +1,13 @@
+package com.example.highbee.model;
+
+public class User {
+
+    private String email;
+    private String password;
+
+    User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+}
