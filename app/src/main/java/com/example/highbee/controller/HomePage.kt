0a2801b551b2f5fa3e298971382ea69b45778f10
GSperandio <gustavo.sperandio25@gmail.com>
2023-09-25 17:06:47 -0300

    Implemented navbottom

--- app/src/main/java/com/example/highbee/controller/HomePage.kt
+++ app/src/main/java/com/example/highbee/controller/HomePage.kt
@@ -2,11 +2,32 @@ package com.example.highbee.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.highbee.R
+import com.example.highbee.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomePageBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.findNavController();
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
     }
 }
