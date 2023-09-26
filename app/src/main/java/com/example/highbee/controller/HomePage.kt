7e24ec7682a20b62d414e0e8097e1365a4a53c7d
GSperandio <gustavo.sperandio25@gmail.com>
2023-09-26 10:26:22 -0300

    Implemented bottom nav - complete

--- app/src/main/java/com/example/highbee/controller/HomePage.kt
+++ app/src/main/java/com/example/highbee/controller/HomePage.kt
@@ -1,13 +1,16 @@
 package com.example.highbee.controller
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.highbee.R
 import com.example.highbee.databinding.ActivityHomePageBinding
+import com.google.android.material.badge.BadgeDrawable
 
 class HomePage : AppCompatActivity() {
 
@@ -22,6 +25,7 @@ class HomePage : AppCompatActivity() {
         setContentView(view)
 
         initNavigation()
+        initBadge()
     }
 
     private fun initNavigation(){
@@ -29,5 +33,16 @@ class HomePage : AppCompatActivity() {
 
         navController = navHostFragment.findNavController();
         NavigationUI.setupWithNavController(binding.bottomNav, navController)
+    }
+
+    private fun initBadge(){
+        var badge = binding.bottomNav.getOrCreateBadge(R.id.menu_cart)
+
+        badge.isVisible = true
+
+        badge.number = 99
+        badge.backgroundColor = getColor(R.color.yellowStrong)
+        badge.badgeTextColor = getColor(R.color.black)
+        //badge.horizontalOffset = 15
     }
 }
