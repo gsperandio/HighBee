5d05e1bfd2734a8b4c7a8d2ae10568b6f8906d8e
GSperandio <gustavo.sperandio25@gmail.com>
2023-09-26 17:42:07 -0300

    Commited Login page

---
+++ app/src/main/java/com/br/highbee/controller/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.br.highbee.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.highbee.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
